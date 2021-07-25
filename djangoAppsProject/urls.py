bf1711ccafff0429779d9313cc41fc92f426bc9e
Ender Siziba <siziba.uz@outlook.com>
2021-07-25 11:31:56 +0200

    make it better

--- djangoAppsProject/urls.py
+++ djangoAppsProject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('inceptus/', include('inceptus.urls')),
+    path('', include('inceptus.urls')),
     path('admin/', admin.site.urls),
 ]
